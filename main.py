f6473fcb594a35bd92485a3342e241cbcc75b6d8
plubpe <78285973+plubpe@users.noreply.github.com>
2021-09-19 11:41:25 +0700

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -7,14 +7,72 @@ tiles.place_on_random_tile(Ai,assets.tile("""black"""))
 g = game.ask_for_number("what is your pin code?")
 c = 0
 x = 0
+stop = 0
+a = 0
+b = 0
+d = 0
+e = 0
+f = 0
+g = 0
+h = 0
+i = 0
+j = 0
+o = 0
+
     
 def on_forever():
+    global g,c,x,stop,a,b,d,e,f,g,h,i,j,o
+    if stop == 1:
+        a=c
+        x =randint(0, 9999)
+        c=1
+    elif stop == 2:
+        b=c
+        x =randint(0, 9999)
+        c=1
+    elif stop == 3:
+        d=c
+        x =randint(0, 9999)
+        c=1
+    elif stop == 4:
+        o = (a+b+c+d)/4
+        Ai.say(o)
+        return
+    #elif stop == 4:
+        #e=c
+        #x =randint(0, 9999)
+        #c=1
+    #elif stop == 5:
+        #f=c
+        #x =randint(0, 9999)
+        #c=1
+    #elif stop == 6:
+        #g=c
+        #x =randint(0, 9999)
+        #c=1
+    #elif stop == 7:
+        #h=c
+        #x =randint(0, 9999)
+        #c=1
+    #elif stop == 8:
+        #i=c
+        #x =randint(0, 9999)
+        #c=1
+    #elif stop == 9:
+        #j=c
+        #x =randint(0, 9999)
+        #c=1
+    #elif stop == 10:
+        #o = a+b+c+d+e+f+g+h+i+j/10
+        #Ai.say(o)
+        #return
     if x != g :
         x =randint(0, 9999)
-    elif x==g :
-        Ai.say(c)
-    
-    c=c+1
-    if c==999:
-        Ai.say("I am exhausted")
+        Ai.say(x)
+        c=c+1
+    if x==g :
+        stop = stop+1
+    #if c==999:
+        #Ai.say("I am exhausted")
+        #stop = 1
 forever(on_forever)
--- main.ts
+++ main.ts
@@ -7,18 +7,76 @@ tiles.placeOnRandomTile(Ai, assets.tile`black`)
 let g = game.askForNumber("what is your pin code?")
 let c = 0
 let x = 0
+let stop = 0
+let a = 0
+let b = 0
+let d = 0
+let e = 0
+let f = 0
+g = 0
+let h = 0
+let i = 0
+let j = 0
+let o = 0
+// if c==999:
+// Ai.say("I am exhausted")
+// stop = 1
 forever(function on_forever() {
-    let x: number;
-    let c: number;
-    if (x != g) {
+    
+    if (stop == 1) {
+        a = c
         x = randint(0, 9999)
-    } else if (x == g) {
-        Ai.say(c)
+        c = 1
+    } else if (stop == 2) {
+        b = c
+        x = randint(0, 9999)
+        c = 1
+    } else if (stop == 3) {
+        d = c
+        x = randint(0, 9999)
+        c = 1
+    } else if (stop == 4) {
+        o = (a + b + c + d) / 4
+        Ai.say(o)
+        return
     }
     
-    c = c + 1
-    if (c == 999) {
-        Ai.say("I am exhausted")
+    // elif stop == 4:
+    // e=c
+    // x =randint(0, 9999)
+    // c=1
+    // elif stop == 5:
+    // f=c
+    // x =randint(0, 9999)
+    // c=1
+    // elif stop == 6:
+    // g=c
+    // x =randint(0, 9999)
+    // c=1
+    // elif stop == 7:
+    // h=c
+    // x =randint(0, 9999)
+    // c=1
+    // elif stop == 8:
+    // i=c
+    // x =randint(0, 9999)
+    // c=1
+    // elif stop == 9:
+    // j=c
+    // x =randint(0, 9999)
+    // c=1
+    // elif stop == 10:
+    // o = a+b+c+d+e+f+g+h+i+j/10
+    // Ai.say(o)
+    // return
+    if (x != g) {
+        x = randint(0, 9999)
+        Ai.say(x)
+        c = c + 1
+    }
+    
+    if (x == g) {
+        stop = stop + 1
     }
     
 })
